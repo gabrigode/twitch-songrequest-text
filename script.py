5d9f93c8182d2838361a48d7473b97634307cf28
Gabriel Barbosa <gbarbosa7841@outlook.com>
2020-01-07 13:23:15 -0300

    Removes geckodriver need

--- script.py
+++ script.py
@@ -1,27 +1,31 @@
-import urllib.request
-from bs4 import BeautifulSoup
-from selenium import webdriver
+from urllib.request import urlopen
+import json
+import requests
 import time
-import os, json
+import os
 
 print ('--------------------------TWITCH-SONGREQUEST-WRITE-----------------------------------------------------')
-print ('Opening browser...')
-driver = webdriver.Firefox(executable_path=r'PATH WHERE YOU PUT GECKODRIVER.EXE')
-driver.get ('YOUR SONG REQUEST URL')
+
+channelName = input('Channel name:')
+
+channel_url = f'https://api.nightbot.tv/1/channels/t/{channelName}'
+channel_load = urlopen(channel_url)
+
+jsonList = json.load(channel_load)
+channel_id = jsonList['channel']['_id']
+print (f"Channel's id: {channel_id}")
+
+def Music():
+    r = requests.get("https://api.nightbot.tv/1/song_requests/queue", headers={"Nightbot-Channel":channel_id})
+    musicName = (r.json()['_currentSong']['track']['title'])
+    print (f"Playing: {musicName}")
+    f = open('music.txt', 'w', encoding='utf-8')
+    f.write(musicName)
+    f.close()
 
 while True:
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    list_name = soup.find('h4')
-    print(list_name)
-    name = list_name.text.strip()
-    name2 = name.replace ("—", "-")
-    name2.rstrip()
-    print(name2)
-    print ('Writing music name on txt file...')
-    f = open('music.txt', 'w', encoding='utf-8')
-    f.write(name2)
-    f.close()
-    driver.refresh()
-    print ('Refreshing page...')
-    time.sleep(5)
+    Music()
+    print ("Refreshing page...")
+    time.sleep(10)
+    
+
